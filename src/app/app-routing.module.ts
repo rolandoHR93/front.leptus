93379ccdb728c165341b7a88f82990e6ab3f55f8
Rolando HR <rolando167@hotmail.com>
2021-12-21 08:39:38 -0500

    ca

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './pages/errors/page-not-found/page-not-found.component';
-// import { HomePageComponent } from './pages/home/home-page/home-page.component';
 import { HomeRoutingModule } from './pages/home/home-page/home.routing';
 
 const routes: Routes = [
-    // { path: '/auth', redirectTo: '/auth', pathMatch: 'full'},
-
-
     { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: '**', component: PageNotFoundComponent },
+
     {
         path: 'login',
-        loadChildren: () =>
-          import('./pages/auth/login/login.module').then(
-            (m) => m.LoginModule
-          ),
+        loadChildren: ()=>
+            import('./pages/auth/login/login.module')
+                .then(mod => mod.LoginModule),
     },
-    // { path: '', component: HomePageComponent },
-    // { path: 'home', component: HomePageComponent },
-    { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
