2f512640cbf7e14d8147e830b4bb6d478f98257e
Rolando HR <rolando167@hotmail.com>
2021-12-23 10:16:43 -0500

    ca

---
+++ src/app/core/config/constants.ts
@@ -0,0 +1,14 @@
+export class AppSettings {
+
+    // public static API_ENDPOINT ='http://localhost:8000/api';
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    public static API_ENDPOINT ='https://serene-brook-26274.herokuapp.com/api';
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    public static CODE_API ='iT3BnOENtV30pxRDadZ99e43wbDL4NA9';
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    public static API_ENDPOINT_LOCAL ='http://127.0.0.1:8000/api';
+
+  }
