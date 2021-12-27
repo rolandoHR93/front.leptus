c1d9400e3b1da44b5f6ee38cf2cc22296a2a9572
Rolando HR <rolando167@hotmail.com>
2021-12-27 11:58:54 -0500

    ca

--- src/app/modules/admin/example/example.component.ts
+++ src/app/modules/admin/example/example.component.ts
@@ -1,16 +1,21 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenStorageService } from 'app/core/services/auth/token-storage.service';
 
 @Component({
     selector     : 'example',
     templateUrl  : './example.component.html',
     encapsulation: ViewEncapsulation.None
 })
-export class ExampleComponent
+export class ExampleComponent  implements OnInit
 {
-    /**
-     * Constructor
-     */
-    constructor()
-    {
+    currentUser: any;
+
+    constructor(private _token: TokenStorageService,
+        private _router: Router,
+        ) { }
+
+    ngOnInit(): void {
+        this.currentUser = this._token.getUser();
     }
 }
