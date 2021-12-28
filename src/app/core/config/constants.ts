bd7dce0ebe9fa0311e62c1500e1c836bf09d3951
Rolando HR <rolando167@hotmail.com>
2021-12-28 17:32:10 -0500

    ca

--- src/app/core/config/constants.ts
+++ src/app/core/config/constants.ts
@@ -3,9 +3,13 @@ export class AppSettings {
 
     // public static API_ENDPOINT ='http://localhost:8000/api';
 
-    public static API_ENDPOINT ='https://serene-brook-26274.herokuapp.com/api';
+    // public static API_ENDPOINT ='https://serene-brook-26274.herokuapp.com/api';
 
-    public static API_END_SERVICE ='http://api.leptus.pe/api';
+    // public static API_END_SERVICE ='https://api.leptus.pe/api';
+
+
+    // ----------------------------
+    public static API_END_SERVICE ='http://localhost:8000/api';
 
     public static CODE_API ='iT3BnOENtV30pxRDadZ99e43wbDL4NA9';
 
