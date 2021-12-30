f15c09c8a53458f0dcd953bd1c31e02956b668e8
Rolando HR <rolando167@hotmail.com>
2021-12-30 12:38:42 -0500

    ca

--- src/app/modules/auth/sign-up/sign-up.component.ts
+++ src/app/modules/auth/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FuseValidators } from '@fuse/validators';
 
 @Component({
     selector     : 'forms-wizards',
@@ -32,33 +33,41 @@ export class AuthSignUpComponent implements OnInit
         // Vertical stepper form
         this.verticalStepperForm = this._formBuilder.group({
             step1: this._formBuilder.group({
-                nombres : ['', Validators.required],
-                apellido_paterno : ['', Validators.required],
-                apellido_materno : ['', Validators.required],
+                nombres : ['a', Validators.required],
+                apellido_paterno : ['a', Validators.required],
+                apellido_materno : ['a', Validators.required],
                 // --------
-                tipo_documento : ['', Validators.required],
-                nro_documento : ['', Validators.required],
-                sexo : ['', Validators.required],
-                fecha_nacimiento : ['', Validators.required],
-                direccion : ['', Validators.required],
-                ubigeo : ['', Validators.required],
+                tipo_documento : ['2', Validators.required],
+                nro_documento : ['a', Validators.required],
+                sexo : ['1', Validators.required],
+                fecha_nacimiento : ['a', Validators.required],
+                direccion : ['a', Validators.required],
+                ubigeo : ['a', Validators.required],
             }),
             step2: this._formBuilder.group({
-                razon_social: ['', Validators.required],
-                nombre_comercial: ['', Validators.required],
-                tipo_documento: ['', Validators.required],
-                nro_documento : ['', Validators.required],
-                nro_contacto : ['', Validators.required],
-                direccion : ['', Validators.required],
-                ubigeo : ['', Validators.required],
+                razon_social: ['a', Validators.required],
+                nombre_comercial: ['a', Validators.required],
+                tipo_documento: ['1', Validators.required],
+                nro_documento : ['a', Validators.required],
+                nro_contacto : ['a', Validators.required],
+                direccion : ['a', Validators.required],
+                ubigeo : ['a', Validators.required],
                 // --------
             }),
+
             step3: this._formBuilder.group({
-                email   : ['', [Validators.required, Validators.email]],
-                password       : ['', Validators.required],
-                passwordConfirm: ['', Validators.required]
-            })
-        });
+                email   : [' ', [Validators.required, Validators.email]],
+                password: ['', Validators.required],
+                passwordConfirm : ['', Validators.required],
+                // --------
+            },
+            {
+                validators: FuseValidators.mustMatch('password', 'passwordConfirm')
+            }
+            ),
+        }
+        );
+
     }
 
     resetPassword(): void
