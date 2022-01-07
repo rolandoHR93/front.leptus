51519ecb45582b6a14c7806728fa1d5810108340
Rolando HR <rolando167@hotmail.com>
2022-01-07 17:31:20 -0500

    ca

--- src/app/modules/auth/sign-up/sign-up.service.ts
+++ src/app/modules/auth/sign-up/sign-up.service.ts
@@ -1,12 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AppSettings } from 'app/core/config/constants';
 import { Observable } from 'rxjs';
 
-
 const AUTH_API = AppSettings.API_END_SERVICE;
 const API_ENDPOINT_LOCAL = AppSettings.API_ENDPOINT_LOCAL;
 const CODE_API = AppSettings.CODE_API;
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'content-type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -27,4 +30,11 @@ export class SignUpService {
 		return this._httpClient.get<any[]>(AUTH_API + '/ubigeos/getDistritos/' + CODE_API+'/'+deparatamentoID+'/'+provinciaID);
 	}
 
+    register(nombres: string, email: string, password: string): Observable<any> {
+        return this._httpClient.post(AUTH_API  + '/auth/' + 'register/' + CODE_API, {
+        nombres,
+        email,
+        password
+        }, httpOptions);
+    }
 }
