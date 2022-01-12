a1123d896c7ece794afd30b90f409d27a3db672d
Rolando HR <rolando167@hotmail.com>
2022-01-12 15:38:20 -0500

    cambios

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -3,8 +3,98 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
+        id      : 'dashboards',
+        title   : 'Dashboard',
+        subtitle: 'Acceso Rápido',
+        type    : 'group',
+        icon    : 'heroicons_outline:home',
+        children: [
+            {
+                id   : 'dashboards.administracion',
+                title: 'Administración',
+                type : 'collapsable',
+                icon : 'heroicons_outline:menu-alt-2',
+                children: [
+                    {
+                        id   : 'dashboards.administracion.usuarios',
+                        title: 'Usuarios',
+                        type : 'basic',
+                        icon : 'heroicons_outline:user-group',
+                        link : '/dashboards/administracion/usuarios',
+                    },
+                    {
+                        id   : 'dashboards.administracion.cliene',
+                        title: 'Cliente',
+                        type : 'basic',
+                        icon : 'heroicons_outline:minus-sm',
+                        link : '/dashboards/administracion/cliene',
+                    },
+                    {
+                        id   : 'dashboards.administracion.personalInterno',
+                        title: 'Personal Interno',
+                        type : 'basic',
+                        icon : 'heroicons_outline:minus-sm',
+                        link : '/dashboards/administracion/personalInterno',
+                    },
+                    {
+                        id   : 'dashboards.administracion.servicios',
+                        title: 'Servicios',
+                        type : 'basic',
+                        icon : 'heroicons_outline:minus-sm',
+                        link : '/dashboards/administracion/servicios',
+                    },
+                    {
+                        id   : 'dashboards.administracion.modulos',
+                        title: 'Módulos',
+                        type : 'basic',
+                        icon : 'heroicons_outline:minus-sm',
+                        link : '/dashboards/administracion/modulos',
+                    },
+                    {
+                        id   : 'dashboards.administracion.permisos',
+                        title: 'Permisos',
+                        type : 'basic',
+                        icon : 'heroicons_outline:minus-sm',
+                        link : '/dashboards/administracion/permisos',
+                    },
+                ]
+            },
+            {
+                id   : 'dashboards.ventas',
+                title: 'Ventas',
+                type : 'basic',
+                icon : 'heroicons_outline:cash',
+                link : '/dashboards/ventas'
+            },
+            {
+                id   : 'dashboards.facturacion',
+                title: 'Facturación',
+                type : 'basic',
+                icon : 'heroicons_outline:currency-dollar',
+                link : '/dashboards/facturacion'
+            },
+            {
+                id   : 'dashboards.analytics',
+                title: 'Análisis',
+                type : 'basic',
+                icon : 'heroicons_outline:chart-pie',
+                link : '/dashboards/analytics'
+            },
+            {
+                id   : 'dashboards.settings',
+                title: 'Settings',
+                type : 'basic',
+                icon : 'heroicons_outline:cog',
+                link : '/dashboards/settings'
+            }
+        ]
+    },
+];
+
+export const nodefaultNavigation: FuseNavigationItem[] = [
+    {
         id   : 'example',
-        title: 'Example',
+        title: 'Example defaultNavigation',
         type : 'basic',
         icon : 'heroicons_outline:chart-pie',
         link : '/example'
@@ -13,7 +103,7 @@ export const defaultNavigation: FuseNavigationItem[] = [
 export const compactNavigation: FuseNavigationItem[] = [
     {
         id   : 'example',
-        title: 'Example',
+        title: 'Example compactNavigation',
         type : 'basic',
         icon : 'heroicons_outline:chart-pie',
         link : '/example'
@@ -22,7 +112,7 @@ export const compactNavigation: FuseNavigationItem[] = [
 export const futuristicNavigation: FuseNavigationItem[] = [
     {
         id   : 'example',
-        title: 'Example',
+        title: 'Example futuristicNavigation',
         type : 'basic',
         icon : 'heroicons_outline:chart-pie',
         link : '/example'
@@ -31,7 +121,7 @@ export const futuristicNavigation: FuseNavigationItem[] = [
 export const horizontalNavigation: FuseNavigationItem[] = [
     {
         id   : 'example',
-        title: 'Example',
+        title: 'Example horizontalNavigation',
         type : 'basic',
         icon : 'heroicons_outline:chart-pie',
         link : '/example'
